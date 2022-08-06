fbb97290901630bd96a401f3447582e6e4f7ce22
daknya_bam <daknyabam18@gmil.com>
2022-08-06 14:54:53 +0530

    fa

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+var darkmode = false;
+
+
+
 const button = document.getElementById("mod");
 const bod = document.querySelector('.for_mod');
 const projectBox = document.querySelectorAll('.project-box');
+const blogs = document.querySelectorAll('.artical');
+
 
 button.addEventListener('click',()=>{
 
@@ -8,5 +14,9 @@ button.addEventListener('click',()=>{
     projectBox.forEach(query=>{
         query.classList.toggle('darkmodep');
     });
+    blogs.forEach(query=>{
+        query.classList.toggle('darkmodep');
+    });
     button.classList.toggle('back');
+    darkmode = true;
 });
