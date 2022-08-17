6e708cba8d7dfa1da9c7b8a381dbfffe9cf2d0a2
daknya_bam <daknyabam18@gmil.com>
2022-08-17 23:42:26 +0530

    g

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-var darkmode = false;
-
-
 
 const button = document.getElementById("mod");
 const bod = document.querySelector('.for_mod');
 const projectBox = document.querySelectorAll('.project-box');
 const blogs = document.querySelectorAll('.artical');
+let mode = localStorage.getItem("dark-mode");
+ const enabledark=()=>{
+    bod.classList.add('darkmode');
+    projectBox.forEach(query=>{
+        query.classList.add('darkmodep');
+    });
+    blogs.forEach(query=>{
+        query.classList.add('darkmodep');
+    });
+    button.classList.add('back');
+    localStorage.setItem("dark-mode", "enabled");
+};
+
+
+const disabledark=()=>{
+    bod.classList.remove('darkmode');
+    projectBox.forEach(query=>{
+        query.classList.remove('darkmodep');
+    });
+    blogs.forEach(query=>{
+        query.classList.remove('darkmodep');
+    });
+    button.classList.remove('back');
+    localStorage.setItem("dark-mode", "disabled");
+};
+
 
 
 button.addEventListener('click',()=>{
-
-    bod.classList.toggle('darkmode');
-    projectBox.forEach(query=>{
-        query.classList.toggle('darkmodep');
-    });
-    blogs.forEach(query=>{
-        query.classList.toggle('darkmodep');
-    });
-    button.classList.toggle('back');
-    darkmode = true;
+      mode = localStorage.getItem("dark-mode");
+        if (mode === "disabled") {
+          enabledark();
+        } else {
+          disabledark();
+        }
 });
